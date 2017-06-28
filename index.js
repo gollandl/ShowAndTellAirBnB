a5731a1baa05e5bb2a7088e3cbfa09b02a819243
gollandl <lydia.golland@britishgas.co.uk>
2017-06-28 09:18:00 +0100

    Navigate from Screen One to Screen Two

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 import Navigator from 'native-navigation';
-import { SCREENONE } from './routes';
+import { SCREENONE, SCREENTWO } from './routes';
 
-Navigator.registerScreen(SCREENONE, () => require('./screens/ScreenOne'));
+
+Navigator.registerScreen(
+  SCREENONE,
+  () => require('./screens/ScreenOne'),
+  {
+    initialConfig: {
+      title: 'Screen One',
+    },
+  }
+);
+
+Navigator.registerScreen(
+  SCREENTWO,
+  () => require('./screens/ScreenTwo'),
+  {
+    initialConfig: {
+      title: 'Screen Two',
+    },
+  }
+);
