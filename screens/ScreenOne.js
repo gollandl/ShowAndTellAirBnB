28193513448df15fa7ded994d079799f6b831cfe
Elliot Hanger <hangere@hangere.local>
2017-06-28 14:56:50 +0100

    adapted boilerplate example to a hard coded version of our POC dummy app

--- screens/ScreenOne.js
+++ screens/ScreenOne.js
@@ -5,6 +5,8 @@ import {
   Text,
   View,
   Button,
+  Image,
+  TextInput,
 } from 'react-native';
 import Navigator, { TabBar, Tab } from 'native-navigation';
 import Screen from '../components/Screen';
@@ -12,15 +14,24 @@ import { SCREENTWO } from '../routes';
 
 
 export default class ScreenOne extends Component {
+
   render() {
     return (
       <Screen>
         <View style={styles.container}>
+
+
           <Text style={styles.welcome}>
             Screen One
           </Text>
+
+          <Image style ={styles.pic} source={require('../images/bg.png')}/>
+
+          <Text>Please login</Text>
+          <TextInput style={styles.textInput} placeholder={'Username'} placeholderTextColor={"rgba(198,204,1)"}/>
+          <TextInput style={styles.textInput} placeholder={'Password'} placeholderTextColor={"rgba(198,204,1)"}secureTextEntry ref={(input) => this.passwordInput = input}/>
           <Button
-            title="Push"
+            title="Log in"
             onPress={() => Navigator.push(SCREENTWO)}
           />
         </View>
@@ -45,4 +56,29 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+
+  pic:{
+
+  },
+textInput:{
+  justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 65,
+        height: 20,
+        width: 250,
+        borderWidth: 1,
+        borderColor: "rgba(0,0,0,0.5)"
+},
+
+pic: {
+    flexGrow:1,
+    width:380,
+    height:150,
+    alignSelf:'center',
+    flexBasis:'27%',
+    marginBottom:50,
+    marginLeft:10,
+  },
+
+
 });
---
+++ screens/ScreenThree.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+import Navigator from 'native-navigation';
+import { SCREENFOUR } from '../routes';
+import {SCREENTWO} from '../routes';
+import Screen from '../components/Screen';
+
+ class ScreenThree extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text style={styles.text}>Smart Card number:12345678912345678</Text>
+
+        <Text style={styles.list}> Recent transactions</Text>
+        <Text style={styles.subheading}>This years amounts:</Text>
+        <View syle={styles.transactions}>
+          <Text style={styles.transactions}>19 Jan    2017                    £22.00</Text>
+          <Text style={styles.transactions}>30 Feb   2017                    £19.50</Text>
+          <Text style={styles.transactions}>15 Mrch 2017                    £17.25</Text>
+          <Text style={styles.transactions}>03 Apl   2017                    £25.99</Text>
+          <Text style={styles.transactions}>28 Apl   2017                    £17.56</Text>
+        </View>
+          <Button
+            title="Top Up"
+            onPress={() => Navigator.push(SCREENTWO)}
+          />
+
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    alignItems: 'center',
+    marginTop:70,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  text: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5
+    },
+    balence:{
+    //flex:1,
+    flexDirection:'row',
+    marginBottom:10,
+
+
+    },
+
+    cost:{
+      marginTop:20,
+    marginLeft:50,
+    fontSize:25,
+    color:'navy'
+
+    },
+
+    bold:{
+    marginTop:30,
+    marginRight:100,
+    marginLeft:20,
+    fontSize:20,
+    color:'black',
+    },
+indent:{
+  alignSelf:'flex-start',
+  marginLeft:20,
+},
+list:{
+  marginLeft:30,
+  fontSize:20,
+  color:'navy',
+
+},
+
+transactions:{
+
+  marginLeft:20,
+  marginBottom:10,
+
+},
+subheading:{
+  fontSize:15,
+  marginBottom:10,
+},
+
+
+});
+export default ScreenThree;
